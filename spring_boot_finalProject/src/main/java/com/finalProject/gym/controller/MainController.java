38ab93a5f0b2090e49204108c90b2b2baa079aac
KDH <maxol2558@gmail.com>
2024-09-20 18:09:23 +0900

    update 2024.09.20

--- spring_boot_finalProject/src/main/java/com/finalProject/gym/controller/MainController.java
+++ spring_boot_finalProject/src/main/java/com/finalProject/gym/controller/MainController.java
@@ -412,9 +412,9 @@ public class MainController {
 	@RequestMapping("/member/imageFileUpload")
 	public String imageFileUpload(@RequestParam("uploadFile") MultipartFile file, HttpSession session)
 			throws IOException {
-		String uploadPath = "C:/springWorkspace/upload/";
+		//String uploadPath = "C:/springWorkspace/upload/";
 		// 이미지 업로드 서버 경로
-		//String uploadPath = "/usr/local/project/upload/";
+		String uploadPath = "/usr/local/project/upload/";
 
 		String originalFileName = file.getOriginalFilename();				
 		
@@ -445,8 +445,8 @@ public class MainController {
 	@RequestMapping("/member/imageFileUploadOnly")
 	public void imageFileUploadOnly(@RequestParam("uploadFile") MultipartFile file, HttpSession session)
 			throws IOException {
-		String uploadPath = "C:/springWorkspace/upload/";
-		//String uploadPath = "/usr/local/project/upload/";
+		//String uploadPath = "C:/springWorkspace/upload/";
+		String uploadPath = "/usr/local/project/upload/";
 
 		String originalFileName = file.getOriginalFilename();
 
@@ -460,8 +460,8 @@ public class MainController {
 	@RequestMapping("/member/imageFileUploadMulti")
 	public ArrayList<String> imageFileUploadMulti(@RequestParam("uploadFile") MultipartFile[] files, HttpSession session)
 			throws IOException {
-		String uploadPath = "C:/springWorkspace/upload/";
-		//String uploadPath = "/usr/local/project/upload/";
+		//String uploadPath = "C:/springWorkspace/upload/";
+		String uploadPath = "/usr/local/project/upload/";
 		
 		ArrayList<String> fileNameList = new ArrayList<String>();
 		
